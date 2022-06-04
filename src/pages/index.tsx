053154a42c6624e08e7db2c31fad868a5b49208c
DJ1TJOO <t.ferb1@gmail.com>
2022-06-05 01:54:48 +0200

    Changed back to img

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,6 @@ import { Popover, Transition } from '@headlessui/react';
 import { ChevronRightIcon, MenuIcon, XIcon } from '@heroicons/react/outline';
 
 import type { NextPage } from 'next';
-import Image from 'next/image';
 import { useRouter } from 'next/router';
 import { Fragment, useEffect, useMemo, useState } from 'react';
 import Scholar from '../../serp-wrapper';
@@ -40,7 +39,7 @@ const Home: NextPage = () => {
 							<div className="flex items-center justify-between w-full md:w-auto">
 								<a href="#">
 									<span className="sr-only">Workflow</span>
-									<Image className="h-8 w-auto sm:h-10" src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg" alt="" />
+									<img className="h-8 w-auto sm:h-10" src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg" alt="" />
 								</a>
 								<div className="-mr-2 flex items-center md:hidden">
 									<Popover.Button className="bg-gray-900 rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:bg-gray-800 focus:outline-none focus:ring-2 focus-ring-inset focus:ring-white">
@@ -81,7 +80,7 @@ const Home: NextPage = () => {
 						<div className="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
 							<div className="px-5 pt-4 flex items-center justify-between">
 								<div>
-									<Image className="h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="" />
+									<img className="h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="" />
 								</div>
 								<div className="-mr-2">
 									<Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-600">
@@ -168,7 +167,7 @@ const Home: NextPage = () => {
 							</div>
 							<div className="mt-12 -mb-16 sm:-mb-48 lg:m-0 lg:relative">
 								<div className="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 lg:max-w-none lg:px-0">
-									<Image className="w-full lg:absolute lg:inset-y-0 lg:left-0 lg:h-full lg:w-auto lg:max-w-none" src="home-icon.svg" alt="" />
+									<img className="w-full lg:absolute lg:inset-y-0 lg:left-0 lg:h-full lg:w-auto lg:max-w-none" src="home-icon.svg" alt="" />
 								</div>
 							</div>
 						</div>
