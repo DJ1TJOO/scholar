a8d6cc8c65bdce352fa331fb0a511961f722a9aa
DJ1TJOO <t.ferb1@gmail.com>
2022-06-16 20:54:28 +0200

    Removed bold

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -229,7 +229,7 @@ const Home: NextPage = () => {
 							<div className="lg:grid lg:grid-cols-2 lg:gap-8">
 								<div className="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 sm:text-center lg:px-0 lg:text-left lg:flex lg:items-center">
 									<div className="lg:py-24">
-										<h1 className="mt-4 text-4xl tracking-tight font-extrabold text-white sm:mt-5 sm:text-6xl lg:mt-6 xl:text-6xl">
+										<h1 className="mt-4 text-4xl tracking-tight font-bold text-white sm:mt-5 sm:text-6xl lg:mt-6 xl:text-6xl">
 											<span className="">
 												Een <span className=" text-indigo-400">betere</span> manier om <span className=" text-indigo-400">scholar</span> te{' '}
 												<span className=" text-indigo-400">zoeken</span>
