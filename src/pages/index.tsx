829669c79ee9b703970656411e02555cfb7a99de
DJ1TJOO <t.ferb1@gmail.com>
2022-06-16 19:50:39 +0200

    Fix search

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -252,11 +252,7 @@ const Home: NextPage = () => {
 												<div className="mt-3 sm:mt-0 sm:ml-3">
 													<button
 														onClick={() => {
-															router.push('/', {
-																query: {
-																	search: query,
-																},
-															});
+															window.location.href = '/?search=' + query;
 														}}
 														className="block w-full py-3 px-4 rounded-md shadow bg-indigo-500 text-white font-medium hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-300 focus:ring-offset-gray-900"
 													>
